a0786566e15c06e1b9cf6b378a1b863099657dbe
bijuacharju <bijuacharju4@gmail.com>
2023-03-31 10:14:11 +0545

    fix: image data type changed to LONGTEXT

--- models/Product.js
+++ models/Product.js
@@ -1,39 +1,38 @@
 // for using in another files
 
 module.exports = (sequelize, DataTypes) => {
-    // // Table name as Blog in double quote and Blog as variable in front
-    const Product = sequelize.define("Product", {
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  // // Table name as Blog in double quote and Blog as variable in front
+  const Product = sequelize.define("Product", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
 
-        description: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        price: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
 
-        image: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
+    image: {
+      type: DataTypes.TEXT("long"),
+      allowNull: true,
+    },
 
-        quantity: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
+
+  Product.associate = (models) => {
+    Product.hasMany(models.Order, {
+      onDelete: "cascade",
     });
+  };
 
-    Product.associate = (models) => {
-        Product.hasMany(models.Order, {
-            onDelete: "cascade",
-        });
-    };
-   
-
-    return Product;
+  return Product;
 };
